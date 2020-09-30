272f489bd4425a8e9d7b9fe75acd28ad8daeed67
Mais <59611735+itsmais@users.noreply.github.com>
2020-09-30 13:14:01 +0300

    fixed width

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,55 +12,68 @@
 //     'London',
 //     'Tokyo'
 // ]
-
-let timeZoneLinks = [
-    'https://worldtimeapi.org/api/timezone/asia/beirut', // link to time for specific IP
-    'https://worldtimeapi.org/api/timezone/asia/shanghai',
-    'https://worldtimeapi.org/api/timezone/europe/moscow',
-    'https://worldtimeapi.org/api/timezone/america/toronto',
-    'https://worldtimeapi.org/api/timezone/australia/melbourne',
-    'https://worldtimeapi.org/api/timezone/europe/madrid',
-    'https://worldtimeapi.org/api/timezone/europe/berlin',
-    'https://worldtimeapi.org/api/timezone/asia/seoul',
-    'https://worldtimeapi.org/api/timezone/europe/madrid',
-    'https://worldtimeapi.org/api/timezone/america/new_york',
-    'https://worldtimeapi.org/api/timezone/europe/london',
-    'https://worldtimeapi.org/api/timezone/asia/tokyo'
-];
+let utc_offsets=[
+    'user city',
+    "+08:00",
+    '+03:00',
+    '-04:00',
+    "+10:00",
+    "+02:00",
+    "+02:00",
+    '+09:00',
+    '+02:00',
+    "-04:00",
+    '+01:00"',
+    '+09:00'
+]
+let timeZoneLink = 'https://worldtimeapi.org/api/ip';
+    // https://worldtimeapi.org/api/timezone/asia/shanghai
+    // https://worldtimeapi.org/api/timezone/europe/moscow
+    // https://worldtimeapi.org/api/timezone/america/toronto
+    // https://worldtimeapi.org/api/timezone/australia/melbourne
+    // https://worldtimeapi.org/api/timezone/europe/madrid
+    // https://worldtimeapi.org/api/timezone/europe/berlin
+    // https://worldtimeapi.org/api/timezone/asia/seoul
+    // https://worldtimeapi.org/api/timezone/europe/madrid
+    // https://worldtimeapi.org/api/timezone/america/new_york
+    // https://worldtimeapi.org/api/timezone/europe/london
+    // https://worldtimeapi.org/api/timezone/asia/tokyo
 
 let times = [];
 
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  
+  fetch("https://worldtimeapi.org/api/ip", requestOptions)
+    .then(response => response.text())
+    .then(result => {
+        let parsedResult = JSON.parse(result);
+        let indOfSlash = parsedResult["timezone"].indexOf('/')+1;
+        let localRegion = parsedResult["timezone"].substring(indOfSlash);
+        let localUTCTime = parsedResult["utc_datetime"];
+        updateLocalRegion(localRegion);
 
-function loadFile(url, timeout, callback) {
-    var args = Array.prototype.slice.call(arguments, 3);
-    var xhr = new XMLHttpRequest();
-    xhr.ontimeout = function () {
-        console.error("The request for " + url + " timed out.");
-    };
-    xhr.onload = function () {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                callback.apply(xhr, args);
-            } else {
-                console.error(xhr.statusText);
-            }
+        ////////////////////////////////////////////
+        for (let i=0; i<12; i++){
+            // utc time, utc offsets
+            let temp = new Date (localUTCTime) + utc_offsets[i];
+            updateTime(temp.toUTCString(),i);
         }
-    };
-    xhr.open("GET", url, true);
-    xhr.timeout = timeout;
-    xhr.send(null);
+        ////////////////////////////////////////////
+
+        
+    })
+    .catch(error => console.log('error', error));
+
+
+function updateLocalRegion(region){
+    document.getElementById("c0").innerHTML=region;
 }
-let i = 0;
-
-function showMessage(message) {
-    console.log(this.responseText);
-    times.push(JSON.parse(this.responseText)["datetime"].substring(11, 19));
-    document.getElementById("c" + i).innerHTML += times[0];
-    i++;
+function updateTime(time, country_index){
+    document.getElementById("c"+country_index).innerHTML+=time;
 }
-
-// loadFile("https://worldtimeapi.org/api/timezone/asia/tokyo", 2000, showMessage, "");
-
-for (let j in timeZoneLinks) {
-    loadFile(timeZoneLinks[j], 2000, showMessage, "");
+function getTime (dataTimeString){
+    return (dataTimeString.substring(11,19));
 }
